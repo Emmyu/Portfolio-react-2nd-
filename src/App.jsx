26dd69ef78ee3f87099a4170ac45bb37b2cf4262
Just Emmy <87806620+Emmyu@users.noreply.github.com>
2024-04-16 15:40:24 +0100

    Updated font style for portfolio title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar"; // Remove .jsx
 import { Hero } from "./components/Hero"; // Remove .jsx
+import About from "./components/About";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
